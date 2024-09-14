08fdf8596388c9c00bb603f76e96953db891f5b7
David Stainton <dstainton415@gmail.com>
2024-09-13 19:07:54 -0700

    Fixup bench, remove average overhead

--- Bench.lean
+++ Bench.lean
@@ -5,7 +5,7 @@ structure Bench :=
   acc : Nat
   _start : Nat
   N : Nat
-  overhead : Nat
+
 namespace Bench
 
 def new : Bench :=
@@ -13,7 +13,6 @@ def new : Bench :=
     acc := 0
     _start := 0
     N := 1000
-    overhead := 0
   }
 
 def stop (b : Bench) : IO Bench := do
@@ -23,7 +22,6 @@ def stop (b : Bench) : IO Bench := do
         acc := b.acc + (now-b._start)
         _start := 0
         N := b.N
-        overhead := b.overhead
       }
 
 def start (b : Bench) : IO Bench := do
@@ -33,14 +31,28 @@ def start (b : Bench) : IO Bench := do
         acc := b.acc
         _start := now
         N := b.N
-        overhead := b.overhead
       }
 
+def overheadAvg : IO Nat := do
+  let mut b := Bench.new
+  for _ in [0:b.N] do
+    b ← b.start
+    b ← b.stop
+  return b.acc / b.N
+
 def report (b : Bench) (name : String) : IO Unit := do
-  let avg := b.acc / b.N
-  let coloredAvg := (Colorized.color Color.Magenta (Colorized.style Style.Bold s!"{avg}"))
-  let s := s!"{name}        {b.N} iter      {coloredAvg} ns/op"
+  let avgNano := (b.acc / b.N) - (← overheadAvg)
+  let msecs := avgNano / 1000000
+  let secs := msecs / 1000
+
+  let coloredNano := (Colorized.color Color.Magenta (Colorized.style Style.Bold s!"{avgNano}"))
+  let coloredMsecs := (Colorized.color Color.Red (Colorized.style Style.Bold s!"{msecs}"))
+  let coloredName := (Colorized.style Style.Bold s!"{name}")
+  let iters := (Colorized.style Style.Underline s!"{b.N} iter")
+
+
+  let s := s!"{coloredName}        {iters}     {coloredNano} ns/op  {coloredMsecs} ms/op  {secs} s/op"
+
   IO.println s
 
 end Bench
-
--- lakefile.lean
+++ lakefile.lean
@@ -6,3 +6,5 @@ require Colorized from git "https://github.com/axiomed/Colorized.lean.git"
 package "Bench" where
 
 lean_lib Bench where
+
+lean_exe Bench.Example
